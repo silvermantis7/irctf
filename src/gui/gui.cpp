064b55c1550e302509bea970b6d5171fa50b106b
James Gobbie <james.gobbie@null.net>
2025-07-04 19:02:21 +1200

    Add word wrapping in MessageDisplay.
    Wrap words if they are 5 pixels away from the edge of the MessageDisplay
    box.

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
 
 #ifndef _WIN32
 #include <fontconfig/fontconfig.h>
@@ -307,23 +308,45 @@ void MessageDisplay::draw()
     window.blContext.setFillStyle(textColor);
     window.blContext.clipToRect(BLRect(posX, posY, width, height));
 
+    BLGlyphBuffer glyphBuffer;
+    BLTextMetrics textMetrics;
+    blFont.shape(glyphBuffer);
+    glyphBuffer.setUtf8Text(" ");
+    blFont.getTextMetrics(glyphBuffer, textMetrics);
+
+    const double spaceWidth = textMetrics.advance.x;
+
+    // timestamp, nick, messages
+    typedef std::tuple<std::string, std::string, std::vector<std::string>>
+        PrintMessage;
+    std::vector<PrintMessage> printMessages;
+
+    int offsetYTest = offsetY;
+    double offsetXText = offsetX;
+
+    int nLines = 0;
+    int nNewlines = 0;
+
     for (Message message : std::vector(messages.begin() +
         std::floor(messagesScrolled), messages.end()))
     {
+        PrintMessage printMessage;
+
+        if (nLines - nNewlines > maxMessagesVisible)
+        {
+            break;
+        }
+
         tm* timeInfo = localtime(&std::get<0>(message));
         std::string msgText('[' + std::to_string(timeInfo->tm_hour) + ':' +
             std::to_string(timeInfo->tm_min) + ':' +
             std::to_string(timeInfo->tm_sec) + ']');
-        BLGlyphBuffer glyphBuffer;
         glyphBuffer.setUtf8Text(msgText.c_str());
-        blFont.shape(glyphBuffer);
-        BLTextMetrics textMetrics;
         blFont.getTextMetrics(glyphBuffer, textMetrics);
+        std::get<0>(printMessage) = msgText;
 
-        window.blContext.fillUtf8Text(BLPoint(posX + offsetX, posY + offsetY),
-            blFont, msgText.c_str());
-
-        double nextPosXCandidate = posX + offsetX + textMetrics.advance.x + 10;
+        double nextPosXCandidate = posX + offsetXText + textMetrics.advance.x +
+            10;
 
         if (nextPosXCandidate > nickPosX)
         {
@@ -337,9 +360,7 @@ void MessageDisplay::draw()
         msgText = std::string('<' + std::get<1>(message) + '>');
         glyphBuffer.setUtf8Text(msgText.c_str());
         blFont.getTextMetrics(glyphBuffer, textMetrics);
-
-        window.blContext.fillUtf8Text(BLPoint(nickPosX, posY + offsetY), blFont,
-            msgText.c_str());
+        std::get<1>(printMessage) = msgText.c_str();
 
         nextPosXCandidate += textMetrics.advance.x + 10;
 
@@ -348,18 +369,76 @@ void MessageDisplay::draw()
             msgPosX = nextPosXCandidate;
         }
 
-        window.blContext.fillUtf8Text(BLPoint(msgPosX, posY + offsetY), blFont,
-            std::get<2>(message).c_str());
+        std::string word;
+        std::stringstream ss(std::get<2>(message));
 
-        if (offsetY + lineHeight >= posY + height)
+        if (!std::getline(ss, word, ' '))
         {
-            break;
+            continue;
         }
 
-        offsetY += lineHeight;
+        glyphBuffer.setUtf8Text(word.c_str());
+        blFont.getTextMetrics(glyphBuffer, textMetrics);
+
+        double wordPosX = msgPosX + textMetrics.advance.x + spaceWidth;
+        int msgIndex = 0;
+        int lastMsgIndex = 0;
+        nLines++;
+
+        ss.str(std::get<2>(message));
+
+        while (std::getline(ss, word, ' '))
+        {
+            glyphBuffer.setUtf8Text(word.c_str());
+            blFont.getTextMetrics(glyphBuffer, textMetrics);
+
+            if (wordPosX + textMetrics.advance.x >= posX + width - 5)
+            {
+                nLines++;
+                nNewlines++;
+                std::get<2>(printMessage).emplace_back(
+                    std::get<2>(message).substr(lastMsgIndex, msgIndex -
+                    lastMsgIndex - 1));
+                lastMsgIndex = msgIndex;
+
+                offsetYTest += lineHeight;
+
+                wordPosX = msgPosX;
+            }
+
+            wordPosX += textMetrics.advance.x + spaceWidth;
+            msgIndex += word.length() + 1;
+        }
+
+        std::get<2>(printMessage).emplace_back(std::get<2>(message).substr(
+            lastMsgIndex));
+
+        printMessages.push_back(std::move(printMessage));
+
+        offsetYTest += lineHeight;
     }
 
-    if (messages.size() > std::floor(maxMessagesVisible))
+    double wrapOverflowShift{offsetYTest > height ? (offsetYTest - height) *
+        scrollPercent : 0};
+
+    for (PrintMessage& printMessage : printMessages)
+    {
+        double printY = posY + offsetY - wrapOverflowShift;
+        window.blContext.fillUtf8Text(BLPoint(posX + offsetX, printY), blFont,
+            std::get<0>(printMessage).c_str());
+        window.blContext.fillUtf8Text(BLPoint(nickPosX, printY), blFont,
+            std::get<1>(printMessage).c_str());
+
+        for (std::string& message : std::get<2>(printMessage))
+        {
+            window.blContext.fillUtf8Text(BLPoint(msgPosX, printY), blFont,
+                message.c_str());
+            printY += lineHeight;
+            offsetY += lineHeight;
+        }
+    }
+
+    if (nLines > std::floor(maxMessagesVisible))
     {
         double scrollbarLen = height * maxMessagesVisible /
             (double)messages.size();
